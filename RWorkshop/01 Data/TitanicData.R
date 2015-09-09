744f76a030affae6226bb59afed03e9023d56e3f
Rolando Melendez <rolando.melendez@utexas.edu>
2015-09-09 16:40:48 -0500

    This adds the Titanic Data File to 01 Data. It contains only dummy data that will be changed later

---
+++ RWorkshop/01 Data/TitanicData.R
@@ -0,0 +1,47 @@
+require("jsonlite")
+require("RCurl")
+
+# CThis is just to test the student oracle database account)
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_rm46926', PASS='orcl_rm46926', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+summary(df)
+head(df)
+#Currently not working ^ ... just kidding it works... now
+
+require(extrafont)
+require(ggplot2)
+
+#This is actually the first require plot :)
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  #facet_grid(PCLASS~SURVIVED) +
+  labs(title='Titanic') +
+  labs(x="Age", y=paste("Fare")) +
+  layer(data=df, 
+        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
+#Not sure what this plot is, its not required just keeping it here as a guide to look at also it gives errors but finishes.
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_grid(PCLASS~SURVIVED) +
+  labs(title='Titanic') +
+  labs(x="SURVIVED", y=paste("FARE")) +
+  layer(data=df, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
