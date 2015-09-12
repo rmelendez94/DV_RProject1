46fe44619f5a7c82e2c46f1e04686647479b7325
Rolando Melendez <rolando.melendez@utexas.edu>
2015-09-11 21:18:51 -0500

    Database now up code un commented and removed commented code in datavis 1 file

--- RWorkshop/02 Visualizations/TitanicVisualization_1.R
+++ RWorkshop/02 Visualizations/TitanicVisualization_1.R
@@ -16,21 +16,3 @@ ggplot() +
         geom_params=list(), 
         position=position_jitter(width=0.3, height=0)
   )
-
-#Not sure what this plot is, its not required just keeping it here as a guide to look at also it gives errors but finishes.
-# ggplot() + 
-#   coord_cartesian() + 
-#   scale_x_discrete() +
-#   scale_y_continuous() +
-#   #facet_grid(PCLASS~SURVIVED) +
-#   labs(title='Titanic') +
-#   labs(x="SURVIVED", y=paste("FARE")) +
-#   layer(data=df, 
-#         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
-#         stat="identity", 
-#         stat_params=list(), 
-#         geom="point",
-#         geom_params=list(), 
-#         #position=position_identity()
-#         position=position_jitter(width=0.3, height=0)
-#   )
